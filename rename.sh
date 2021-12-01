9ece3a3746b96a178053f3f942d4c5805aac8229
Jarod Burchill <jburchill@acerta.ai>
2021-11-30 21:10:49 -0500

    Fix scripts to work on macOS

--- rename.sh
+++ rename.sh
@@ -2,10 +2,14 @@
 read -p "Enter repository name: " name
 spaces=$(echo $name | tr "-" " ")
 underscores=$(echo $name | tr "-" "_")
-capitalized=$(sed "s/\b\(.\)/\u\1/g" <<< $spaces)
-sed -i "s/nestjs-example/$name/g" ./package.json
-sed -i "s/nestjs_example/$underscores/g" ./local_setup.sh
-sed -i "s/nestjs_example/$underscores/g" ./.env
-sed -i "s/NestJS Example/$capitalized/g" ./src/main.ts
+capitalized=$(echo $spaces | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1')
+sed -i ".bak" -e "s/nestjs-example/$name/g" ./package.json
+sed -i ".bak" -e "s/nestjs_example/$underscores/g" ./local_db_setup.sh
+sed -i ".bak" -e "s/nestjs_example/$underscores/g" ./.env
+sed -i ".bak" -e "s/NestJS Example/$capitalized/g" ./src/main.ts
+rm ./package.json.bak
+rm ./local_db_setup.sh.bak
+rm ./.env.bak
+rm ./src/main.ts.bak
 npm i
 rm ./rename.sh
