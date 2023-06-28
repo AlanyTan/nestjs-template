dcfdd60084fd08fabce1379e661187c3cc640800
Thomas Brown <tbrown@acerta.ai>
2023-06-28 09:32:49 -0400

    feat(pipelines): generate proper test reports
    Add project configuration in order to generate test reports that can be
    parsed by Azure Pipelines.

--- pipelines/ci-tests.sh
+++ pipelines/ci-tests.sh
@@ -7,5 +7,11 @@ PROJECT_DIR="$(realpath "${SCRIPT_DIR}/..")"
 # Running everything from src/ and failing if we can't
 cd "${PROJECT_DIR}" || exit 1
 
+# Required environment variables
+set -o allexport
+# shellcheck source=/dev/null
+source "${SCRIPT_DIR}/ci.env"
+set +o allexport
+
 npm clean-install --include=dev
-npm run test:ci:unit
+npm run test:ci
